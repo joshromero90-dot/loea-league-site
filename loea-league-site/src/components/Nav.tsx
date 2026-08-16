0ad15b1220affe6b1423d5b76d2a05bd79426e10
joshromero90-dot <josh.romero90@gmail.com>
2026-08-16 16:51:27 -0600

    Increase logo height in Nav component

--- loea-league-site/src/components/Nav.tsx
+++ loea-league-site/src/components/Nav.tsx
@@ -43,7 +43,7 @@ export default async function Nav() {
               alt="The League of Extraordinary Assholes"
               width={480}
               height={313}
-              className="h-24 w-auto"
+              className="h-32 w-auto"
               priority
             />
           </Link>
